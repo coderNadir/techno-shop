14af7ee3be3d02c556c9d3ebb67917461a8d426d
BM-Nadir <nadir.kerroum@benchmatrix.ca>
2023-04-05 02:18:00 +0100

    initial commit, create categories - category components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './categories.styles.scss';
+import CategoriesMenu from './components/categories-menu/categories-menu.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const categories = [
+		{
+			id: 1,
+			title: 'laptops',
+			imageURL: 'https://i.ibb.co/2SNC3JM/laptops.png',
+		},
+		{
+			id: 2,
+			title: 'montiors',
+			imageURL: 'https://i.ibb.co/yYjMJBQ/monitors.png',
+		},
+		{
+			id: 3,
+			title: 'keyboards',
+			imageURL: 'https://i.ibb.co/YfNbcJP/keyboards.png',
+		},
+		{
+			id: 4,
+			title: 'desks',
+			imageURL: 'https://i.ibb.co/GFX5hBJ/desks.png',
+		},
+		{
+			id: 5,
+			title: 'accessories',
+			imageURL: 'https://i.ibb.co/KFtV8vZ/accessories.png',
+		},
+	];
+
+	return <CategoriesMenu categories={categories} />;
+};
 
 export default App;
